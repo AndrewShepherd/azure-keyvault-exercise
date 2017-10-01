031f4f3b2a6f0d794e9c0e6c459858ba633bbd27
Andrew Shepherd <shepherd_a@hotmail.com>
2017-10-01 19:58:46 +1000

    First bundle of code to get working

---
+++ azure-keyvault-exercise/App_Start/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Azure.KeyVault;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(azure_keyvault_exercise.App_Start.Startup))]
+
+namespace azure_keyvault_exercise.App_Start
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder appBuilder)
+		{
+			var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(EncryptionHelper.GetToken));
+
+			Task<Microsoft.Azure.KeyVault.Models.SecretBundle> task = kv.GetSecretAsync(WebConfigurationManager.AppSettings["SecretUri"]);
+
+			task.ContinueWith(continuation =>
+			{
+				Microsoft.Azure.KeyVault.Models.SecretBundle secretBundle = continuation.Result;
+				EncryptionHelper.EncryptSecret = secretBundle.Value;
+			});
+		}
+	}
+}
